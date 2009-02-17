b548439ab2e285444448ac959368ad6ad34eaa0f
Ehren Murdick <ehren@theedgecase.com>
2009-02-17 11:11:37 -0500

    removed views, made short part of links generate from id

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,20 +1,12 @@
 class Link < ActiveRecord::Base
   
-  validates_presence_of :short, :long
-  validates_uniqueness_of :short, :message => 'URL has already been taken.'
-  
-  before_save :generate_short, :if => Proc.new{|l|l.new_record?}
-  
-  def generate_short
-    symbols = '0123456789abcdefgihjklmnopqrstuvwxyz'
-    while self.short.blank? or ![self,nil].include?(Link.find_by_short(self.short)) do
-      srand
-      self.short = symbols[rand(symbols.size),1] + symbols[rand(symbols.size),1] + symbols[rand(symbols.size),1] + symbols[rand(symbols.size),1] + symbols[rand(symbols.size),1] + symbols[rand(symbols.size),1]
-    end
-    return short
+  validates_presence_of :long
+
+  def self.find_by_short(short)
+    short.to_i(36)
   end
-  
-  def id
-    short
+
+  def short
+    self[:id].to_s(36)
   end
 end
